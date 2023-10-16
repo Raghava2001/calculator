5bea084e24edc64f473c9cfd091b30d72d5ff20f
VVR GitHub <vvrcaj@gmail.com>
2023-10-16 16:00:25 -0400

    this is my initial commit calculator

---
+++ src/main/java/raghava/App.java
@@ -0,0 +1,53 @@
+package raghava;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        
+        double num2 = input.nextDouble();
+
+        System.out.println("\nSelect an operation:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+
+        System.out.print("Enter the operation (1/2/3/4): ");
+        int operation = input.nextInt();
+
+        double result;
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case 2:
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case 3:
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case 4:
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero is not allowed.");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                }
+                break;
+            default:
+                System.out.println("Invalid operation selection.");
+        }
+    }
+}
+
